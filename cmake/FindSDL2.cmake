a66a28b0d9434848d08b3aa59fa0d10ecc61474e
Lectem <lectem@gmail.com>
2016-02-27 14:10:40 -0500

    fix FindSDL2.cmake licence

--- cmake/FindSDL2.cmake
+++ cmake/FindSDL2.cmake
@@ -52,18 +52,6 @@
 # is #include "SDL.h", not <SDL2/SDL.h>. This is done for portability
 # reasons because not all systems place things in SDL2/ (see FreeBSD).
 
-#=============================================================================
-# Copyright 2003-2009 Kitware, Inc.
-#
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-# (To distribute this file outside of CMake, substitute the full
-#  License text for the above reference.)
 
 message("<FindSDL2.cmake>")
 
